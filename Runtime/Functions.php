1e64c45e4f6743d8f6b386ca519f9218db809ef7
Damian Bushong <damian@emberlabs.org>
2012-02-04 09:45:51 -0600

    convert this to a function, don't need a full-blown object for it

--- Runtime/Functions.php
+++ Runtime/Functions.php
@@ -19,3 +19,17 @@
  */
 
 namespace emberlabs\shot\Runtime;
+use \emberlabs\openflame\Event\Dispatcher;
+use \emberlabs\openflame\Event\Instance as Event;
+use \emberlabs\shot\WebKernel;
+
+function hook($name, array &$data)
+{
+	$hook = Event::newEvent($name)
+		->setData($data);
+
+	$kernel = WebKernel::getInstance();
+	$kernel->dispatcher->trigger($hook, Dispatcher::TRIGGER_MIXEDBREAK);
+
+	$data = $hook->getData();
+}
